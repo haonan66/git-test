6dd82c022cd4aed479b1ed58393470cb85d3073f
lhn <731331417@qq.com>
2021-10-13 20:35:03 +0800

    first commit

---
+++ src/main/java/com/lhn/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.lhn.git;
+
+/**
+ * @author Haonan
+ * @date 2021/10/13 20:31
+ * @Email:731331417@qq.com
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
